2c9c25fd7fa4d5379bc5f854106cf0d0372c1cae
RahafOthman <=>
2023-08-20 16:29:18 +0200

    edit products

--- src/Modules/Product/Product.router.js
+++ src/Modules/Product/Product.router.js
@@ -15,25 +15,25 @@ router.post('/:productId/addToWishList', auth(Object.values(roles)), asyncHandle
 router.delete('/:productId/removeFromWishList', auth(Object.values(roles)), asyncHandler(ProductController.removeProductFromWishList));
 
 
-router.post('/', auth(Object.values(roles)),fileUpload(fileValidation.image).fields([
+router.post('/', auth(roles.Admin),fileUpload(fileValidation.image).fields([
     { name:'mainImage', maxCount:1 },
     { name:'subImages', maxCount:5 }
 ]),validation(validators.createProduct),asyncHandler(ProductController.createProduct));
 //endPoint.create
 //endPoint.update
-router.patch('/:productId',auth(Object.values(roles)), fileUpload(fileValidation.image).fields([
+router.patch('/:productId',auth(roles.Admin), fileUpload(fileValidation.image).fields([
     {name: 'mainImage', maxsCount:1},
     {name: 'subImages', maxCount:5}
 ]),validation(validators.updateProduct) ,asyncHandler(ProductController.updateProduct));
 
-router.patch('/softDelete/:productId', auth(Object.values(roles)),validation(validators.softDelete) ,asyncHandler(ProductController.softDelete));
+router.patch('/softDelete/:productId', auth(roles.Admin),validation(validators.softDelete) ,asyncHandler(ProductController.softDelete));
 //endPoint.softDelete
-router.delete('/forceDelete/:productId', auth(Object.values(roles)),validation(validators.forceDelete), asyncHandler(ProductController.forceDelete));
+router.delete('/forceDelete/:productId', auth(roles.Admin),validation(validators.forceDelete), asyncHandler(ProductController.forceDelete));
 //endPoint.forceDelete
-router.patch('/restore/:productId', auth(Object.values(roles)),validation(validators.restore), asyncHandler(ProductController.restore));
+router.patch('/restore/:productId', auth(roles.Admin),validation(validators.restore), asyncHandler(ProductController.restore));
 //endPoint.restore
 //endPoint.softDelete
-router.get('/softDelete', auth(Object.values(roles)), asyncHandler(ProductController.getSoftDeleteProducts));//soft deleted products must show for admin only 
+router.get('/softDelete', auth(roles.Admin), asyncHandler(ProductController.getSoftDeleteProducts));//soft deleted products must show for admin only 
 router.get('/:productId', auth(Object.values(roles)),validation(validators.getProduct),asyncHandler(ProductController.getProduct));
 router.get('/', auth(Object.values(roles)), asyncHandler(ProductController.getProducts));
 
