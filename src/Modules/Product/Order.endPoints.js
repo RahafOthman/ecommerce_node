f46451afaa740a28fe8b9a7b6c4b22d2cb897d1b
RahafOthman <=>
2023-08-20 16:41:40 +0200

    new

--- src/Modules/Product/Order.endPoints.js
+++ src/Modules/Product/Order.endPoints.js
@@ -3,6 +3,5 @@ import { roles } from "../../Middleware/auth.middleware.js";
 export const endPoint = {
     create : [roles.User],
     update: [roles.User],
-    cancel: [roles.User],
-    updateStatus: [roles.Admin],
+    delete: [roles.User],
 }
